00f07413bb8e8456a41edbc5a6e583414c16febc
Simão Lopes <simaolopes98@gmail.com>
2024-01-24 13:32:54 +0000

    Update Network task

--- src/network/Network.cpp
+++ src/network/Network.cpp
@@ -2,6 +2,7 @@
 
 namespace Network
 {
+    const char *TAG = "Network";
 
     esp_err_t write_cred_nvs(const char *newSsid, const char *newPassword)
     {
@@ -13,14 +14,14 @@ namespace Network
         //Save SSID
         esp_err_t err = nvs_open(key_word_ssid, NVS_READWRITE, &my_handle);
         if (err != ESP_OK) {
-            Serial.printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+            service_log(TAG,"Error (%s) opening NVS handle!\n", esp_err_to_name(err));
             return err;
         }
         nvs_set_str(my_handle,key_word_ssid,newSsid);
         err = nvs_commit(my_handle);
         if (err != ESP_OK)
         {
-            Serial.printf("Error saving new SSID");
+            service_log(TAG,"Error saving new SSID");
             return err;
         }
         nvs_close(my_handle);
@@ -28,14 +29,14 @@ namespace Network
         //Save Password
         esp_err_t err = nvs_open(key_word_pass, NVS_READWRITE, &my_handle);
         if (err != ESP_OK) {
-            Serial.printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+            service_log(TAG,"Error (%s) opening NVS handle!\n", esp_err_to_name(err));
             return err;
         }
         nvs_set_str(my_handle,key_word_pass,newPassword);
         err = nvs_commit(my_handle);
         if (err != ESP_OK)
         {
-            Serial.printf("Error saving new Password");
+            service_log(TAG,"Error saving new Password");
             return err;
         }
         nvs_close(my_handle);
@@ -43,18 +44,27 @@ namespace Network
         return err;
     }
 
-    char *read_SSID_nvs()
+    esp_err_t read_string_nvs(const char * key_word, char *string)
     {
-
-    }
-
-    char *read_Pass_nvs()
-    {
-
+        nvs_handle_t my_handle;
+        //Save SSID
+        esp_err_t err = nvs_open(key_word, NVS_READWRITE, &my_handle);
+         if (err != ESP_OK) {
+            service_log(TAG,"Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+            strcpy(string,"\0"); //Returns empty string
+            return err;
+        }
+        size_t size = 0;
+        nvs_get_str(my_handle,key_word,string, &size);
+        service_log(TAG,"String read = %s", string);
+        return ESP_OK;
     }
 
     void setup_wifi()
     {
+        char SSID[32];
+        char Password[32];
+
         esp_err_t err = nvs_flash_init();
         if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
             // NVS partition was truncated and needs to be erased
@@ -64,32 +74,49 @@ namespace Network
         }
 
         WiFi.mode(WIFI_AP_STA);
-        Serial.println("Starting AP");
-        Serial.println(ENV_AP_SSID);
+        service_log(TAG,"Starting AP");
+        service_log(TAG,ENV_AP_SSID);
 
         WiFi.softAP(ENV_AP_SSID, ENV_AP_PASSWORD);
 
-        Serial.println("");
-        Serial.println("Connected to WiFi");
+        service_log(TAG,"Connected to WiFi");
         Serial.print("AP IP address: ");
-        Serial.println(WiFi.softAPIP());
+        service_log(TAG,"%s",WiFi.softAPIP().toString());
 
         #if !ENV_SOLO_AP_MODE
-            Serial.println();
-            Serial.print("Connecting to STA");
-            Serial.println(ENV_SSID);
-
-            WiFi.begin(ENV_SSID, ENV_PASSWORD);
-
-            while (WiFi.status() != WL_CONNECTED)
+            read_string_nvs("creds_newSsid",SSID);
+            read_string_nvs("creds_newSsid",Password);
+            if(strlen(SSID) == 0 || strlen(Password) == 0)
             {
-                Serial.println("Network: Trying to connect");
-                vTaskDelay(750/portTICK_PERIOD_MS);
+                service_log(TAG,"Connecting to STA");
+                service_log(TAG,ENV_SSID);
+
+                WiFi.begin(ENV_SSID, ENV_PASSWORD);
+
+                while (WiFi.status() != WL_CONNECTED)
+                {
+                    service_log(TAG,"Network: Trying to connect");
+                    vTaskDelay(750/portTICK_PERIOD_MS);
+                }
+                service_log(TAG,"WiFi connected");
+                service_log(TAG,"IP address: ");
+                service_log(TAG,"%s",WiFi.localIP().toString());
+            }else{
+                service_log(TAG,"Connecting to STA");
+                service_log(TAG,SSID);
+
+                WiFi.begin(SSID, Password);
+
+                while (WiFi.status() != WL_CONNECTED)
+                {
+                    service_log(TAG,"Network: Trying to connect");
+                    vTaskDelay(750/portTICK_PERIOD_MS);
+                }
+                service_log(TAG,"WiFi connected");
+                service_log(TAG,"IP address: ");
+                service_log(TAG,"%s",WiFi.localIP().toString()); 
             }
-            Serial.println("");
-            Serial.println("WiFi connected");
-            Serial.println("IP address: ");
-            Serial.println(WiFi.localIP());
+            
         #endif
     }
 
@@ -98,31 +125,31 @@ namespace Network
         WiFi.disconnect(true,false);
         while (WiFi.status() == WL_CONNECTED)
         {
-            delay(1000);
-            Serial.println("Disconnecting from current Wi-Fi network...");
+            vTaskDelay(1000/portTICK_PERIOD_MS);
+            service_log(TAG,"Disconnecting from current Wi-Fi network...");
         }
-
+        write_cred_nvs(newSsid,newPassword);
         WiFi.begin(newSsid, newPassword);
 
         Serial.print("Connecting to new Wi-Fi network ");
-        Serial.println(newSsid);
+        service_log(TAG,newSsid);
 
         while (WiFi.status() != WL_CONNECTED)//! It can get stuck..
         {
-            delay(1000);
-            Serial.println("Connecting to new Wi-Fi network...");
+            vTaskDelay(1000/portTICK_PERIOD_MS);
+            service_log(TAG,"Connecting to new Wi-Fi network...");
         }
 
-        Serial.println("Connected to new Wi-Fi network");
+        service_log(TAG,"Connected to new Wi-Fi network");
         Serial.print("New STA IP address: ");
-        Serial.println(WiFi.localIP());
+        service_log(TAG,"%s",WiFi.localIP().toString());
     }
 
     void taskNetwork(void *pvParameters)
     {
         setup_wifi();
 
-        Serial.println("Network: Booted");
+        service_log(TAG,"Network: Booted");
         while (true)
         {
             if (uxQueueMessagesWaiting(queues::sta_cred))
--- src/network/Network.hpp
+++ src/network/Network.hpp
@@ -13,6 +13,9 @@ namespace Network
 {
     void setup_wifi();
     void change_STA_cred(const char *newSsid, const char *newPassword);
+    
+    esp_err_t write_cred_nvs(const char *newSsid, const char *newPassword);
+    esp_err_t read_string_nvs(const char * key_word, char *string);
 
     void taskNetwork(void *pvParameters);
 }
